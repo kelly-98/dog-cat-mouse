1788eefaf99433839116858d38c0e33771285655
Tan <lenhattan.1997@gmail.com>
2020-05-08 13:13:06 +0700

    Mouse run method

--- Mouse.js
+++ Mouse.js
@@ -1,8 +1,14 @@
 function Mouse(name) {
 	this.name = name;
-	this.dead = false;
+	this.isDead = false;
 }
+
 Mouse.prototype.die = function() {
-	this.dead = true;
+	this.isDead = true;
 };
+
+Mouse.prototype.run = function() {
+	console.log('Run');
+}
+
 module.exports = Mouse;
